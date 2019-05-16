28e0e4b813cdb5274af2765edcca46dd74aa2245
VTula2000 <36479963+VTula2000@users.noreply.github.com>
2019-05-16 13:31:52 +0100

    Tidy data from updated 2017-18 source file

---
+++ 6.1.1.py
@@ -0,0 +1,58 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# Table 6.1.1: Number of young people in treatment by age (2005-06 to 2017-18)
+
+from gssutils import *
+scraper = Scraper('https://www.gov.uk/government/statistics/substance-misuse-treatment-for-young-people-statistics-2017-to-2018')
+dist = scraper.distribution(title=lambda t: t.startswith('Data tables'))
+tabs = {tab.name: tab for tab in dist.as_databaker()}
+tab = tabs['6.1.1 Trends of Age']
+
+cell = tab.filter('Age')
+cell.assert_one()
+age =  tab.filter('Age').fill(DOWN).is_not_blank().is_not_whitespace()
+period = cell.fill(RIGHT).is_not_blank().is_not_whitespace()
+noobs = tab.filter('n')
+observations = period.fill(DOWN).is_not_blank() - noobs
+
+Dimensions = [
+            HDimConst('Measure Type','Count'),
+            HDim(age,'Clients in treatment',DIRECTLY, LEFT),           
+            HDim(period,'Period',DIRECTLY,ABOVE),
+            HDimConst('Unit','People')            
+            ]
+c1 = ConversionSegment(observations, Dimensions, processTIMEUNIT= True)
+new_table = c1.topandas()
+
+import numpy as np
+new_table['OBS'].replace('', np.nan, inplace=True)
+new_table.dropna(subset=['OBS'], inplace=True)
+new_table.rename(columns={'OBS': 'Value'}, inplace=True)
+new_table['Value'] = new_table['Value'].astype(int)
+
+new_table['Basis of treatment'] =  'Treatment by age' 
+
+new_table['Clients in treatment'] = new_table['Clients in treatment'].map(
+    lambda x: {
+        'Total' : 'All years', 
+        }.get(x, x))
+
+new_table['Substance'] = 'All'
+new_table = new_table[['Period','Basis of treatment','Substance','Clients in treatment','Measure Type','Value','Unit']]
+
+new_table
+
+
